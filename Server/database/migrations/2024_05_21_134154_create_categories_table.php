d6c0e5ac7fe5c992a6b3a64a8d3acdf6faee6dd2
dohaseif2 <dohaseif2@gmail.com>
2024-05-21 17:25:14 +0300

    feat(database): add migrations ( products , users , categories , product_images , settings )

--- Server/database/migrations/2024_05_21_134154_create_categories_table.php
+++ Server/database/migrations/2024_05_21_134154_create_categories_table.php
@@ -11,16 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('gender',['male','female']);
-            $table->string('image');
+            $table->text('description');
             $table->enum('status', ['available', 'unavailable'])->default('available');
-            $table->rememberToken();
+            $table->text('cover_image');
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
