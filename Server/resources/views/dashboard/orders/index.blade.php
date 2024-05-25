f7c16273e0b40d9b9c6f986e37ebf7e65044ec09
dohaseif2 <dohaseif2@gmail.com>
2024-05-25 18:21:20 +0300

    feat(dashboard): Show order items in order table

--- Server/resources/views/dashboard/orders/index.blade.php
+++ Server/resources/views/dashboard/orders/index.blade.php
@@ -31,34 +31,62 @@
                             <strong class="card-title">Orders</strong>
                         </div>
                         <div class="card-body">
-                            <table class="table table-bordered  table-striped">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">User</th>
-                                        <th scope="col">Status</th>
-                                        <th scope="col">Notes</th>
-                                        <th scope="col">Total Price</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($orders as $order)
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-striped">
+                                    <thead>
                                         <tr>
-                                            <td>
-                                                {{ $loop->iteration }}
-                                            </td>
-                                            <td class="text-success fw-bold">{{ $order->user->name }}</td>
-                                            <td class="text-success fw-bold">{{ $order->status }}</td>
-                                            <td class="text-success fw-bold">{{ $order->notes }}</td>
-                                            <td class="text-success fw-bold">{{ $order->total_price }}</td>
-                                            <td>
-
-                                            </td>
+                                            <th scope="col">#</th>
+                                            <th scope="col">User</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col">Notes</th>
+                                            <th scope="col">Total Price</th>
+                                            <th>Actions</th>
                                         </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($orders as $order)
+                                            <tr>
+                                                <td data-toggle="collapse" data-target="#collapse{{ $order->id }}" aria-expanded="false" aria-controls="collapse{{ $order->id }}"><i class="fa fa-plus"></i></td>
+                                                <td class="text-success fw-bold">{{ $order->user->name }}</td>
+                                                <td class="text-success fw-bold">{{ $order->status }}</td>
+                                                <td class="text-success fw-bold">{{ $order->notes }}</td>
+                                                <td class="text-success fw-bold">{{ $order->total_price }}</td>
+                                                <td>
+                                                    <form action="{{ route('orders.destroy', $order->id) }}" method="POST"
+                                                        style="display:inline;">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit"
+                                                            class="btn btn-outline-danger rounded p-2 mx-2">Delete</button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                            <tr id="collapse{{ $order->id }}" class="collapse">
+                                                <td colspan="6">
+                                                    <table class="table table-bordered">
+                                                        <thead>
+                                                            <tr>
+                                                                <th scope="col">Item Name</th>
+                                                                <th scope="col">Quantity</th>
+                                                                <th scope="col">Price</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            @foreach ($order->orderItems as $item)
+                                                                <tr>
+                                                                    <td class="text-success fw-bold">{{ $item->product->title }}</td>
+                                                                    <td class="text-success fw-bold">{{ $item->quantity }}</td>
+                                                                    <td class="text-success fw-bold">{{ $item->price }}</td>
+                                                                </tr>
+                                                            @endforeach
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
