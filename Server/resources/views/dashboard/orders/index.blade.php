9def82efecc4b393968665d0664292d069559010
dohaseif2 <dohaseif2@gmail.com>
2024-05-25 16:23:34 +0300

    style(dashboard) : Change table and button style and add breadcrumb

--- Server/resources/views/dashboard/orders/index.blade.php
+++ Server/resources/views/dashboard/orders/index.blade.php
@@ -1,42 +1,68 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="card">
-    <div class="card-header d-flex justify-content-between">
-        <strong class="card-title">Orders</strong>
+    <div class="breadcrumbs">
+        <div class="col-sm-4">
+            <div class="page-header float-left">
+                <div class="page-title">
+                    <h1>Orders</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-8">
+            <div class="page-header float-right">
+                <div class="page-title">
+                    <ol class="breadcrumb text-right">
+                        <li><a href="#">Dashboard</a></li>
+                        <li><a href="{{ route('orders.index') }}">Orders</a></li>
+                        <li class="active">List Of Orders</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="card-body">
-        <table class="table table-bordered table-dark">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">User</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Notes</th>
-                    <th scope="col">Total Price</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($orders as $order)
-                <tr>
-                    <th>
-                        <a href="javascript:void(0);" onclick="toggleOrderItems({{ $order->id }})">{{ $order->id }}</a>
-                    </th>
-                    <td>{{ $order->user->name }}</td>
-                    <td>{{ $order->status }}</td>
-                    <td>{{ $order->notes }}</td>
-                    <td>{{ $order->total_price }}</td>
-                    <td>
-                        
-                    </td>
-                </tr>
-                
-                @endforeach
-            </tbody>
-        </table>
+
+    <div class="content mt-3">
+        <div class="animated fadeIn">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between">
+                            <strong class="card-title">Orders</strong>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered  table-striped">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">User</th>
+                                        <th scope="col">Status</th>
+                                        <th scope="col">Notes</th>
+                                        <th scope="col">Total Price</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($orders as $order)
+                                        <tr>
+                                            <td>
+                                                {{ $loop->iteration }}
+                                            </td>
+                                            <td class="text-success fw-bold">{{ $order->user->name }}</td>
+                                            <td class="text-success fw-bold">{{ $order->status }}</td>
+                                            <td class="text-success fw-bold">{{ $order->notes }}</td>
+                                            <td class="text-success fw-bold">{{ $order->total_price }}</td>
+                                            <td>
+
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-
-
 @endsection
