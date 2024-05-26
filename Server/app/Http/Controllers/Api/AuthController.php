1e52309c16c16c0dcc74dec7ffeba9c4515a9791
dohaseif2 <dohaseif2@gmail.com>
2024-05-27 00:02:35 +0300

    feat(api): Logout

--- Server/app/Http/Controllers/Api/AuthController.php
+++ Server/app/Http/Controllers/Api/AuthController.php
@@ -4,16 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+
 class AuthController extends Controller
 {
+
     public function login(LoginRequest $request)
     {
-
         $user = User::where('email', $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
@@ -22,10 +24,15 @@ class AuthController extends Controller
             ]);
         }
 
-        $token= $user->createToken($request->device_name)->plainTextToken;
+        $token = $user->createToken($request->device_name)->plainTextToken;
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
         ]);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Logged out successfully']);
     }
 }
