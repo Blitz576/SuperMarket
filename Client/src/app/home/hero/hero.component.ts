2d5353d30be9493b345346451277fde0cc0fd8d8
Ahmedghoraba99 <ahmed.12.2011@gmail.com>
2024-05-26 17:56:14 +0300

    -Cleaned extra slide

--- Client/src/app/home/hero/hero.component.ts
+++ Client/src/app/home/hero/hero.component.ts
@@ -5,32 +5,36 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
   slides = [
     {
-      Quote:"Explore Our Best Products",
-      captionStyle:"carousel-caption-1",
-      imageUrl: 'https://theme905-computer-shop.myshopify.com/cdn/shop/files/img_01_02109cc1-220a-4319-ab36-d43b5340f37a_1296x.png?v=1665429191',
+      Quote: 'Explore Our Best Products',
+      captionStyle: 'carousel-caption-1',
+      imageUrl:
+        'https://theme905-computer-shop.myshopify.com/cdn/shop/files/img_01_02109cc1-220a-4319-ab36-d43b5340f37a_1296x.png?v=1665429191',
       productName: 'First slide label',
-      description: 'Some representative placeholder content for the first slide.'
+      description:
+        'Some representative placeholder content for the first slide.',
     },
     {
-      Quote:"We Are The Best",
-      captionStyle:"carousel-caption",
-      imageUrl: 'https://theme905-computer-shop.myshopify.com/cdn/shop/files/img_34_1296x.png?v=1665467744',
+      Quote: 'We Are The Best',
+      captionStyle: 'carousel-caption',
+      imageUrl:
+        'https://theme905-computer-shop.myshopify.com/cdn/shop/files/img_34_1296x.png?v=1665467744',
       productName: 'Second slide label',
-      description: 'Some representative placeholder content for the second slide.'
+      description:
+        'Some representative placeholder content for the second slide.',
     },
-    {
-      Quote:"Mohamed Ismail Elsayed Harby",
-      captionStyle:"carousel-caption",
-      imageUrl: 'https://scontent.fcai19-3.fna.fbcdn.net/v/t39.30808-6/335299117_159878670216978_3831529047356252070_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=fhMcRbDNEtIQ7kNvgENFdAa&_nc_ht=scontent.fcai19-3.fna&oh=00_AYCItkM_51z9_q9lpryhO6DlLtYPctgmP3MYaCfnsqCUMQ&oe=66581D8B',
-      productName: 'Third slide label',
-      description: 'Some representative placeholder content for the third slide.'
-    }
+    // {
+    //   Quote:"Mohamed Ismail Elsayed Harby",
+    //   captionStyle:"carousel-caption",
+    //   imageUrl: 'https://scontent.fcai19-3.fna.fbcdn.net/v/t39.30808-6/335299117_159878670216978_3831529047356252070_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=fhMcRbDNEtIQ7kNvgENFdAa&_nc_ht=scontent.fcai19-3.fna&oh=00_AYCItkM_51z9_q9lpryhO6DlLtYPctgmP3MYaCfnsqCUMQ&oe=66581D8B',
+    //   productName: 'Third slide label',
+    //   description: 'Some representative placeholder content for the third slide.'
+    // }
   ];
 }
