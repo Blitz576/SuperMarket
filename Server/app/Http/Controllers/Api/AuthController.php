5688b7fce3d4832a6090aa117c0eb24cfadecd09
dohaseif2 <dohaseif2@gmail.com>
2024-05-26 23:55:28 +0300

    feat(api): Login

---
+++ Server/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token= $user->createToken($request->device_name)->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+}
