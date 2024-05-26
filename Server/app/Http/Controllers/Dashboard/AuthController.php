47cb53cf87157384c935614b849d64efe217d15d
dohaseif2 <dohaseif2@gmail.com>
2024-05-26 22:58:49 +0300

    feat(dashboard):Auth Logout

--- Server/app/Http/Controllers/Dashboard/AuthController.php
+++ Server/app/Http/Controllers/Dashboard/AuthController.php
@@ -10,40 +10,47 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(){
+    public function register()
+    {
         return view('authentication.register');
     }
-    public function registerPost(request $request){
+    public function registerPost(request $request)
+    {
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = $request->password;
         $user->mobile_number = $request->mobile_number;
         $user->gender = $request->gender;
-        $user->image="default.jfif";
+        $user->image = "default.jfif";
         $user->status = $request->status ?? 'available';
         $user->save();
 
         return redirect()->route('login');
     }
-    public function login(){
+    public function login()
+    {
         return view('authentication.login');
     }
     public function loginPost(Request $request)
-{
-    $credentials = $request->only('email', 'password');
+    {
+        $credentials = $request->only('email', 'password');
 
-    $user = User::where('email', $credentials['email'])->first();
-    if (!$user) {
-        return back()->withErrors(['email' => 'User does not exist']);
+        $user = User::where('email', $credentials['email'])->first();
+        if (!$user) {
+            return back()->withErrors(['email' => 'User does not exist']);
+        }
+
+        if (Hash::check($credentials['password'], $user->password)) {
+            Auth::login($user);
+            return redirect()->intended(route('products.index'));
+        } else {
+            return back()->withErrors(['email' => 'Invalid credentials']);
+        }
     }
-
-    if (Hash::check($credentials['password'], $user->password)) {
-        Auth::login($user);
-        return redirect()->intended(route('products.index'));
-    } else {
-        return back()->withErrors(['email' => 'Invalid credentials']);
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return redirect()->route('login');
     }
-}
-
 }
--- Server/routes/web.php
+++ Server/routes/web.php
@@ -28,7 +28,7 @@ Route::get('register',[AuthController::class,'register']);
 Route::post('register',[AuthController::class,'registerPost'])->name('register.post');
 Route::get('login',[AuthController::class,'login'])->name('login');
 Route::post('login', [AuthController::class, 'loginPost'])->name('login.post');
-
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 
 Route::get('settings', [SettingController::class, 'edit'])->name('settings.edit');
