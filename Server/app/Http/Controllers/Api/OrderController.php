1f676d7aa2377fd98aa2d20c15870ae1fd22b5f4
dohaseif2 <dohaseif2@gmail.com>
2024-05-27 22:22:38 +0300

    feat(Api):complete checkout

--- Server/app/Http/Controllers/Api/OrderController.php
+++ Server/app/Http/Controllers/Api/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\OrderStoreRequest;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Product;
@@ -11,15 +12,36 @@ use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-    public function store(Request $request)
+    public function store(OrderStoreRequest $request)
     {
         $user = Auth::user();
 
         $order = new Order();
-        $order->user_id = $user->id; 
+        $order->user_id = $user->id;
         $order->cart_id = $request->cart_id;
         $order->status = 'pending';
         $order->notes = $request->notes;
         $order->save();
+        $totalPrice = 0;
+
+        foreach ($request->items as $item) {
+            $product = Product::find($item['product_id']);
+            $price = $product->price;
+
+            $orderItem = new OrderItem();
+            $orderItem->order_id = $order->id;
+            $orderItem->product_id = $item['product_id'];
+            $orderItem->quantity = $item['quantity'];
+            $orderItem->price = $price;
+            $orderItem->save();
+
+            $totalPrice += $price * $item['quantity'];
+        }
+
+        $order->total_price = $totalPrice;
+        $order->save();
+
+        return response()->json(['message' => 'order send successfully'], 200);
     }
+    
 }
