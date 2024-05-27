f34bd909620419e754c8187197c4e144e97edced
dohaseif2 <dohaseif2@gmail.com>
2024-05-28 01:32:28 +0300

    fix(dashboard): show old data in create product form after error occurs

--- Server/resources/views/dashboard/products/create.blade.php
+++ Server/resources/views/dashboard/products/create.blade.php
@@ -40,21 +40,21 @@
                                 <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
                                 <div class="form-group">
                                     <label for="title" class="control-label mb-1">Name</label>
-                                    <input id="title" name="title" type="text" class="form-control">
+                                    <input id="title" name="title" type="text" class="form-control" value="{{ old('title') }}">
                                     @error('title')
                                     <div class="text-danger">{{ $message }}</div>
                                     @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="summary" class="control-label mb-1">Summary</label>
-                                    <input id="summary" name="summary" type="text" class="form-control">
+                                    <input id="summary" name="summary" type="text" class="form-control" value="{{ old('summary') }}">
                                     @error('summary')
                                     <div class="text-danger">{{ $message }}</div>
                                     @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="description" class="control-label mb-1">Description</label>
-                                    <input id="description" name="description" type="text" class="form-control">
+                                    <input id="description" name="description" type="text" class="form-control" value="{{ old('description') }}">
                                     @error('description')
                                     <div class="text-danger">{{ $message }}</div>
                                     @enderror
@@ -73,7 +73,9 @@
                                             <select class="form-control" name="category">
                                                 <option disabled selected>Choose category</option>
                                                 @foreach ($categories as $category)
-                                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                                    <option value="{{ $category->id }}" {{ old('category') == $category->id ? 'selected' : '' }}>
+                                                        {{ $category->name }}
+                                                    </option>
                                                 @endforeach
                                             </select>
                                             @error('category')
@@ -84,7 +86,7 @@
                                     <div class="col-6">
                                         <div class="form-group">
                                             <label for="rate" class="control-label mb-1">Rate</label>
-                                            <input id="rate" name="rate" type="number" class="form-control">
+                                            <input id="rate" name="rate" type="number" class="form-control" value="{{ old('rate') }}">
                                             @error('rate')
                                             <div class="text-danger">{{ $message }}</div>
                                             @enderror
@@ -95,7 +97,7 @@
                                     <div class="col-4">
                                         <div class="form-group">
                                             <label for="price" class="control-label mb-1">Price</label>
-                                            <input id="price" name="price" type="number" class="form-control">
+                                            <input id="price" name="price" type="number" class="form-control" value="{{ old('price') }}">
                                             @error('price')
                                             <div class="text-danger">{{ $message }}</div>
                                             @enderror
@@ -104,7 +106,7 @@
                                     <div class="col-4">
                                         <div class="form-group">
                                             <label for="sale_price" class="control-label mb-1">Sale Price</label>
-                                            <input id="sale_price" name="sale_price" type="number" class="form-control">
+                                            <input id="sale_price" name="sale_price" type="number" class="form-control" value="{{ old('sale_price') }}">
                                             @error('sale_price')
                                             <div class="text-danger">{{ $message }}</div>
                                             @enderror
@@ -113,7 +115,7 @@
                                     <div class="col-4">
                                         <div class="form-group">
                                             <label for="stock" class="control-label mb-1">Stock</label>
-                                            <input id="stock" name="stock" type="number" class="form-control">
+                                            <input id="stock" name="stock" type="number" class="form-control" value="{{ old('stock') }}">
                                             @error('stock')
                                             <div class="text-danger">{{ $message }}</div>
                                             @enderror
@@ -122,14 +124,13 @@
                                 </div>
 
                                 <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" name="show_in_homepage" checked
-                                           id="inlineRadio1"
-                                           value="show">
+                                    <input class="form-check-input" type="radio" name="show_in_homepage" id="inlineRadio1"
+                                           value="show" {{ old('show_in_homepage', 'show') == 'show' ? 'checked' : '' }}>
                                     <label class="form-check-label" for="inlineRadio1">Show in homepage</label>
                                 </div>
                                 <div class="form-check form-check-inline">
                                     <input class="form-check-input" type="radio" name="show_in_homepage" id="inlineRadio2"
-                                           value="hide">
+                                           value="hide" {{ old('show_in_homepage') == 'hide' ? 'checked' : '' }}>
                                     <label class="form-check-label" for="inlineRadio2">Hide from homepage</label>
                                 </div>
                                 @error('show_in_homepage')
