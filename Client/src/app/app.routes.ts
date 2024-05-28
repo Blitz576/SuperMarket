5bfc16db69bb5a65caaf192fc4e2151b33a04a32
Ahmedghoraba99 <ahmed.12.2011@gmail.com>
2024-05-28 13:20:10 +0300

    -Product page route

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -4,22 +4,26 @@ import { ForgetPasswordComponent } from './auth/forget-password/forget-password.
 import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
 import { authenticationLoginGuard } from './guard/authentication-login.guard';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 export const routes: Routes = [
-
-
   //Add  ,canActivate:[authenticationLoginGuard] To Your Critial Route For Authentication
 
   {
-    path: "", redirectTo: "/home", pathMatch: "full"
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
   },
-  { path: "home", component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   {
-    path: "auth",
+    path: 'auth',
     component: AuthComponent,
-    children: [
-      { path: "forget-password", component: ForgetPasswordComponent },
-    ]
+    children: [{ path: 'forget-password', component: ForgetPasswordComponent }],
   },
-  { path: "cart", component: CartComponent ,canActivate:[authenticationLoginGuard] }
+  {
+    path: 'cart',
+    component: CartComponent,
+    canActivate: [authenticationLoginGuard],
+  },
+  { path: 'product/:id', component: ProductPageComponent },
 ];
