9e3d2009f2e5e560d3dfe416679d75482e157cf4
Ahmedghoraba99 <ahmed.12.2011@gmail.com>
2024-05-31 21:30:02 +0300

    -Product page

--- Client/src/app/product-page/product-image-carousel/product-image-carousel.component.ts
+++ Client/src/app/product-page/product-image-carousel/product-image-carousel.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-image-carousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-image-carousel.component.html',
-  styleUrl: './product-image-carousel.component.css'
+  styleUrl: './product-image-carousel.component.css',
 })
 export class ProductImageCarouselComponent {
-
+  @Input() images: string[] = [
+    '../../../assets/iphone.png',
+    'https://placehold.co/800x800',
+    '../../../assets/iphone.png',
+  ];
 }
